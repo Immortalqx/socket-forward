323b3184a62b367ea7abadb6b0e33b0d2d956cca
immortalqx <immortalqx@gmail.com>
2022-12-01 12:16:47 +0800

    update Datagram Test

--- test/Datagram Test/main.cpp
+++ test/Datagram Test/main.cpp
@@ -2,23 +2,28 @@
 #include <cstring>
 #include <cmath>
 
-#define DATALEN 3
+#define DATALEN 8
 
 // 数据报
 typedef struct Datagram
 {
+    // 是否为最后一个数据报
     bool is_end;
-    char buffer[DATALEN];
+    // 保存的数据
+    // NOTE: 这里+1是为了存放'\0'，测试发现如果不这么做，DATALEN取某些值时，recv接受的数据会混乱！
+    char buffer[DATALEN + 1];
 } Datagram;
 
 // 数据报数组
 typedef struct DatagramArray
 {
+    // 数据报长度
     int length;
+    // 数据报数组
     Datagram *datagram;
 } DatagramArray;
 
-//输出数据报数组
+// 输出数据报数组
 void print_datagram(DatagramArray datagramArray)
 {
     for (int i = 0; i < datagramArray.length; i++)
@@ -34,32 +39,31 @@ void print_datagram(DatagramArray datagramArray)
 // NOTE: 在实际情况下，send是一个一个发送数据报，也知道数据的长度，所以输出数据报数组即可！
 DatagramArray send_test(const char *data)
 {
-    //字符串总长
+    // 字符串总长
     int length = strlen(data);
-    //已经处理的字符串长度
+    // 已经处理的字符串长度
     int current_len = 0;
-    //数据报的长度
-    int data_len = ceil(length * 1.0 / (DATALEN - 1));
-
+    // 数据报的长度
+    int data_len = ceil(length * 1.0 / DATALEN);
+    // 数据报数组
     Datagram *datagram = new Datagram[data_len];
 
-    //处理到每一个数据报中
+    // 处理到每一个数据报中
     for (int i = 0; i < data_len; i++)
     {
-        //判断是否为最后一个数据报
+        // 判断是否为最后一个数据报
         if ((data_len - i) == 1)
             datagram[i].is_end = true;
         else
             datagram[i].is_end = false;
-        //打包数据
+        // 打包数据
         int j = 0;
-        for (; j < std::min(DATALEN - 1, length - current_len); j++)
+        for (; j < std::min(DATALEN, length - current_len); j++)
             datagram[i].buffer[j] = data[current_len + j];
         datagram[i].buffer[j] = '\0';
         current_len += j;
     }
-
-    //发布数据
+    // 发布数据
     DatagramArray array;
     array.length = data_len;
     array.datagram = datagram;
@@ -71,41 +75,40 @@ DatagramArray send_test(const char *data)
 // NOTE: recv是一个一个接收数据报，这里要模拟不知道最终数据长度的情况！
 char *recv_test(DatagramArray array)
 {
-    //声明数组
+    // 声明数组
     char *result = nullptr;
     int result_len = 0;
 
-    //目前已经处理的长度
+    // 目前已经处理的长度
     int current_len = 0;
-    //开始循环接收数据！
+    // 开始循环接收数据！
     for (int num = 0; num < array.length; num++)
     {
-        //接收到一个数据报
+        // 接收到一个数据报
         Datagram datagram = array.datagram[num];
 
-        //考虑最后一个数据报长度不固定的情况
+        // 考虑最后一个数据报长度不固定的情况
         if (datagram.is_end)
             result_len = result_len + strlen(datagram.buffer);
         else
             result_len += DATALEN;
-        //实时扩容
+        // 实时扩容
         result = (char *) realloc(result, result_len);
-        //进行数据报处理
+        // 进行数据报处理
         for (int i = 0; i < strlen(datagram.buffer); i++)
         {
             result[current_len++] = datagram.buffer[i];
         }
     }
-    result[current_len] = '\0';
     return result;
 }
 
 [[maybe_unused]] void realloc_test()
 {
-    //如此声明的数组，是否可以被realloc？ 不可以！
+    // 如此声明的数组，是否可以被realloc？ 不可以！
     //char result[DATALEN];
 
-    //声明默认长度的数组
+    // 声明默认长度的数组
     int result_len = DATALEN;
     char *result = (char *) malloc(result_len);
 
@@ -121,7 +124,7 @@ char *recv_test(DatagramArray array)
         result_len += DATALEN;
         result = (char *) realloc(result, result_len);
 
-        //NOTE: 这里记得-1，不然不能够覆盖前面的终结符！
+        // NOTE: 这里记得-1，不然不能够覆盖前面的终结符！
         for (int i = result_len - DATALEN - 1; i < result_len - 1; i++)
             result[i] = 'M';
         result[result_len - 1] = '\0';
